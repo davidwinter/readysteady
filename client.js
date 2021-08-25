f8c62f7671d1264366b0bd0bfcfe62d5ab0ae1b6
David Winter <i@djw.me>
2021-08-25 15:16:54 +0100

    fix: check for GITHUB_TOKEN being set

--- client.js
+++ client.js
@@ -5,4 +5,9 @@ import {restEndpointMethods} from '@octokit/plugin-rest-endpoint-methods';
 import {paginateRest} from '@octokit/plugin-paginate-rest';
 
 const MyOctokit = Octokit.plugin(restEndpointMethods, paginateRest);
+
+if (!process.env.GITHUB_TOKEN) {
+	throw new Error('GITHUB_TOKEN environment variable not detected');
+}
+
 export default new MyOctokit({auth: process.env.GITHUB_TOKEN});
